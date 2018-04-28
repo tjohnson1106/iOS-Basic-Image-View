a1bd09e963e70813d18dc72ba83ef7856d21df90
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-04-28 14:50:08 -0400

    Initial Commit

---
+++ Image-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Image-Example
+//
+//  Created by Thomas Johnson on 4/28/18.
+//  Copyright © 2018 Bracket Factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
